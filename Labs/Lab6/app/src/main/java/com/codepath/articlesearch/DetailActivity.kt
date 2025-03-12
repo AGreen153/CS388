8f83cab71a05a813e5842a282fbdc4b263c56470
AGreen153 <AndrewDennisGreen@gmail.com>
2025-03-12 12:06:43 -0400

    .

--- Labs/Lab6/app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
+++ Labs/Lab6/app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
@@ -9,30 +9,30 @@ import com.bumptech.glide.Glide
 private const val TAG = "DetailActivity"
 
 class DetailActivity : AppCompatActivity() {
-    private lateinit var mediaImageView: ImageView
-    private lateinit var titleTextView: TextView
-    private lateinit var bylineTextView: TextView
-    private lateinit var abstractTextView: TextView
+  private lateinit var mediaImageView: ImageView
+  private lateinit var titleTextView: TextView
+  private lateinit var bylineTextView: TextView
+  private lateinit var abstractTextView: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_detail)
 
-        mediaImageView = findViewById(R.id.mediaImage)
-        titleTextView = findViewById(R.id.mediaTitle)
-        bylineTextView = findViewById(R.id.mediaByline)
-        abstractTextView = findViewById(R.id.mediaAbstract)
+    // TODO: Find the views for the screen
+    mediaImageView = findViewById(R.id.mediaImage)
+    titleTextView = findViewById(R.id.mediaTitle)
+    bylineTextView = findViewById(R.id.mediaByline)
+    abstractTextView = findViewById(R.id.mediaAbstract)
 
-        val article = intent.getSerializableExtra(ARTICLE_EXTRA) as Article
-
-        // Set title and abstract information for the article
-        titleTextView.text = article.headline?.main
-        bylineTextView.text = article.byline?.original
-        abstractTextView.text = article.abstract
-
-        // Load the media image
-        Glide.with(this)
-            .load(article.mediaImageUrl)
-            .into(mediaImageView)
-    }
+    // TODO: Get the extra from the Intent
+    val article = intent.getSerializableExtra(ARTICLE_EXTRA) as DisplayArticle
+    // TODO: Set the title, byline, and abstract information from the article
+    titleTextView.text = article.headline
+    bylineTextView.text = article.byline
+    abstractTextView.text = article.abstract
+    // TODO: Load the media image
+    Glide.with(this)
+      .load(article.mediaImageUrl)
+      .into(mediaImageView)
+  }
 }
